57dccde9f10949f9be7521ab83b4f6f13ef18485
Austin Greer <greer.244@buckeyemail.osu.edu>
2022-11-11 11:23:09 -0500

    Finished edit functionality

--- DatabaseSystemsProject/src/Checkpoint4.java
+++ DatabaseSystemsProject/src/Checkpoint4.java
@@ -262,7 +262,6 @@ public class Checkpoint4 {
         								} catch (SQLException e) {
     									e.printStackTrace();
         								}
-    								
     								break;
     							case "3":
     								System.out.println("--- Add Warehouse ---");
@@ -486,43 +485,310 @@ public class Checkpoint4 {
     							break;
     						case "1":
     							System.out.println("--- Edit Member ---");
-    							//TODO;
+    							System.out.print("Enter the member's ID to edit: ");
+								String currentMemberID = getString(userInput);
+								sql = "UPDATE Member SET Member_ID = ?, First_Name = ?, Last_Name = ?, Address = ?, Email = ?, Start_Date = ?, Warehouse_Distance = ?, Warehouse_Number = ? WHERE Member_ID = '"+currentMemberID+"';";
+								System.out.print("Enter the new member's ID: ");
+								String newMemberID = getString(userInput);
+								System.out.print("Enter the new member's first name: ");
+								String newFirstName = getString(userInput);
+								System.out.print("Enter the new member's last name: ");
+								String newLastName = getString(userInput);
+								System.out.print("Enter the new member's address: ");
+								String newAddress = getString(userInput);
+								System.out.print("Enter the new member's email: ");
+								String newEmail = getString(userInput);
+								System.out.print("Enter the new member's start date: ");
+								String newStartDate = getString(userInput);
+								System.out.print("Enter the new member's distance from the warehouse: ");
+								int newWarehouseDistance = getNum(userInput);
+								System.out.print("Enter the new member's warehouse number: ");
+								int newWarehouseNumber = getNum(userInput);
+								try {
+								PreparedStatement newMember = conn.prepareStatement(sql);
+								newMember.setString(1, newMemberID);
+								newMember.setString(2, newFirstName);
+								newMember.setString(3, newLastName);
+								newMember.setString(4, newAddress);
+								newMember.setString(5, newEmail);
+								newMember.setString(6, newStartDate);
+								newMember.setInt(7, newWarehouseDistance);
+								newMember.setInt(8, newWarehouseNumber);
+								newMember.execute();
+								System.out.println("Member info edited.");
+								} catch (SQLException e) {
+								e.printStackTrace();
+								}
     							break;
     						case "2":
     							System.out.println("--- Edit Equipment ---");
-    							//TODO
+    							System.out.print("Enter the inventory ID to edit: ");
+								String e00 = getString(userInput);
+    							sql = "UPDATE Equipment SET Inv_ID = ?, Warehouse_Number = ?, Description = ?, Active_Category = ?, Model_Number = ?, Type = ?, Year = ?, Serial_Number = ?, Arrival_Date = ?, Warranty_Exp = ?, Manufacturer = ?, Weight = ?, Size = ? WHERE Inv_ID = '"+e00+"';";
+								System.out.print("Enter the new inventory ID: ");
+								String e01 = getString(userInput);
+								System.out.print("Enter the warehouse number: ");
+								int e02 = getNum(userInput);
+								System.out.print("Enter the description: ");
+								String e03 = getString(userInput);
+								System.out.print("Enter the active cateogry: ");
+								String e04 = getString(userInput);
+								System.out.print("Enter the model number: ");
+								String e05 = getString(userInput);
+								System.out.print("Enter the type: ");
+								String e06 = getString(userInput);
+								System.out.print("Enter the active year: ");
+								int e07 = getNum(userInput);
+								System.out.print("Enter the serial number: ");
+								String e08 = getString(userInput);
+								System.out.print("Enter the arrival date: ");
+								String e09 = getString(userInput);
+								System.out.print("Enter the warranty expiration: ");
+								String e10 = getString(userInput);
+								System.out.print("Enter the manufacturer's name: ");
+								String e11 = getString(userInput);
+								System.out.print("Enter the weight: ");
+								int e12 = getNum(userInput);
+								System.out.print("Enter the size: ");
+								int e13 = getNum(userInput);
+								try {
+									PreparedStatement newEquip = conn.prepareStatement(sql);
+									newEquip.setString(1, e01);
+									newEquip.setInt(2, e02);
+									newEquip.setString(3, e03);
+									newEquip.setString(4, e04);
+									newEquip.setString(5, e05);
+									newEquip.setString(6, e06);
+									newEquip.setInt(7, e07);
+									newEquip.setString(8, e08);
+									newEquip.setString(9, e09);
+									newEquip.setString(10, e10);
+									newEquip.setString(11, e11);
+									newEquip.setInt(12, e12);
+									newEquip.setInt(13, e13);
+									newEquip.execute();
+									System.out.println("Equipment info edited.");
+    								} catch (SQLException e) {
+									e.printStackTrace();
+    								}
     							break;
     						case "3":
     							System.out.println("--- Edit Warehouse ---");
-    							//TODO
+    							System.out.print("Enter the warehouse number to edit: ");
+								int currentWarehouseNum = getNum(userInput);
+    							sql = "UPDATE Warehouse SET Warehouse_Number = ?, Manager_Name = ?, Address = ?, Phone_Num = ?, Storage_Capacity = ?, Drone_Capacity = ? WHERE Warehouse_Number = '"+currentWarehouseNum+"';";
+								System.out.print("Enter the new warehouse number: ");
+								int newWarehouseNum = getNum(userInput);
+								System.out.print("Enter the new warehouse manager name: ");
+								String newManagerName = getString(userInput);
+								System.out.print("Enter the new warehouse address: ");
+								String newWarehouseAddress = getString(userInput);
+								System.out.print("Enter the new warehouse phone number: ");
+								long newWarehousePhoneNum = getLongNum(userInput);
+								System.out.print("Enter the new warehouse capacity: ");
+								String newWarehouseCapacity = getString(userInput);
+								System.out.print("Enter the new warehouse drone capacity: ");
+								int newWarehouseDroneCapacity = getNum(userInput);
+								try {
+									PreparedStatement newWarehouse = conn.prepareStatement(sql);
+									newWarehouse.setInt(1, newWarehouseNum);
+									newWarehouse.setString(2, newManagerName);
+									newWarehouse.setString(3, newWarehouseAddress);
+									newWarehouse.setLong(4, newWarehousePhoneNum);
+									newWarehouse.setString(5, newWarehouseCapacity);
+									newWarehouse.setInt(6, newWarehouseDroneCapacity);
+									newWarehouse.execute();
+									System.out.println("Warehouse info edited.");
+    								} catch (SQLException e) {
+									e.printStackTrace();
+    								}
     							break;
     						case "4":
     							System.out.println("--- Edit Drone ---");
-    							//TODO
+    							System.out.print("Enter the current drone serial number to edit: ");
+								String d00 = getString(userInput);
+    							sql = "UPDATE Drone SET Serial_Number = ?, Warehouse = ?, Description = ?, Model_Number = ?, Fleet_ID = ?, Manufacturer = ? WHERE Serial_Number = '"+d00+"';";
+								System.out.print("Enter the new serial number: ");
+								String d01 = getString(userInput);
+								System.out.print("Enter the new warehouse number: ");
+								int d02 = getNum(userInput);
+								System.out.print("Enter the new description: ");
+								String d03 = getString(userInput);
+								System.out.print("Enter the new model number: ");
+								String d04 = getString(userInput);
+								System.out.print("Enter the new fleet ID: ");
+								String d05 = getString(userInput);
+								System.out.print("Enter the new manufacturer: ");
+								String d06 = getString(userInput);
+								try {
+									PreparedStatement newDroneEntry = conn.prepareStatement(sql);
+									newDroneEntry.setString(1, d01);
+									newDroneEntry.setInt(2, d02);
+									newDroneEntry.setString(3, d03);
+									newDroneEntry.setString(4, d04);
+									newDroneEntry.setString(5, d05);
+									newDroneEntry.setString(6, d06);
+									newDroneEntry.execute();
+									System.out.println("Drone info edited.");
+    								} catch (SQLException e) {
+									e.printStackTrace();
+    								}
     							break;
     						case "5":
     							System.out.println("--- Edit Drone Custom Order ---");
-    							//TODO;
+    							System.out.print("Enter the current drone order ID: ");
+								String do00 = getString(userInput);
+    							sql = "UPDATE Custom_Order_Drones SET Order_ID = ?, Drone_Number = ? WHERE Order_ID = '"+do00+"';";
+								System.out.print("Enter the new order ID: ");
+								String do01 = getString(userInput);
+								System.out.print("Enter the new drone serial number: ");
+								String do02 = getString(userInput);
+								try {
+									PreparedStatement newDroneOrder = conn.prepareStatement(sql);
+									newDroneOrder.setString(1, do01);
+									newDroneOrder.setString(2, do02);
+									newDroneOrder.execute();
+									System.out.println("Drone custom order info edited.");
+    								} catch (SQLException e) {
+									e.printStackTrace();
+    								}
     							break;
     						case "6":
     							System.out.println("--- Edit Equipment Custom Order ---");
-    							//TODO
+    							System.out.print("Enter the current equipment order ID: ");
+								String eo00 = getString(userInput);
+    							sql = "UPDATE Custom_Order_Equipment SET Order_ID = ?, Inv_ID = ? WHERE Order_ID = '"+eo00+"';";
+								System.out.print("Enter the new order ID: ");
+								String eo01 = getString(userInput);
+								System.out.print("Enter the new inventory ID: ");
+								String eo02 = getString(userInput);
+								try {
+									PreparedStatement newEquipOrder = conn.prepareStatement(sql);
+									newEquipOrder.setString(1, eo01);
+									newEquipOrder.setString(2, eo02);
+									newEquipOrder.execute();
+									System.out.println("Equipment custom order info edited.");
+    								} catch (SQLException e) {
+									e.printStackTrace();
+    								}
     							break;
     						case "7":
     							System.out.println("--- Edit Other Custom Order ---");
-    							//TODO
+    							System.out.print("Enter the current other order ID: ");
+								String co00 = getString(userInput);
+    							sql = "UPDATE Custom_Order SET Order_ID = ?, Member_ID = ?, Description = ?, Value = ?, ETA = ?, Due_Date = ?, Fee_Charged = ? WHERE Order_ID = '"+co00+"';";
+								System.out.print("Enter the new order ID: ");
+								String co01 = getString(userInput);
+								System.out.print("Enter the new member ID: ");
+								String co02 = getString(userInput);
+								System.out.print("Enter the new description: ");
+								String co03 = getString(userInput);
+								System.out.print("Enter the new value: ");
+								int co04 = getNum(userInput);
+								System.out.print("Enter the new ETA: ");
+								String co05 = getString(userInput);
+								System.out.print("Enter the new due date: ");
+								String co06 = getString(userInput);
+								System.out.print("Enter the new fee charged: ");
+								int co07 = getNum(userInput);
+								try {
+									PreparedStatement newCustomOrder = conn.prepareStatement(sql);
+									newCustomOrder.setString(1, co01);
+									newCustomOrder.setString(2, co02);
+									newCustomOrder.setString(3, co03);
+									newCustomOrder.setInt(4, co04);
+									newCustomOrder.setString(5, co05);
+									newCustomOrder.setString(6, co06);
+									newCustomOrder.setInt(7, co07);
+									newCustomOrder.execute();
+									System.out.println("Other custom order info edited.");
+    								} catch (SQLException e) {
+									e.printStackTrace();
+    								}
     							break;
     						case "8":
     							System.out.println("--- Edit New Element Order ---");
-    							//TODO
+    							System.out.print("Enter the current element order ID: ");
+								String el00 = getString(userInput);
+    							sql = "UPDATE New_Elements_Ordered SET Order_ID = ?, Description = ?, Quantity = ?, Value = ?, ETA = ?, Arrival_Date = ?, Warehouse_Number = ? WHERE Order_ID = '"+el00+"';";
+								System.out.print("Enter the new order ID: ");
+								String el01 = getString(userInput);
+								System.out.print("Enter the new description: ");
+								String el02 = getString(userInput);
+								System.out.print("Enter the new quantity: ");
+								int el03 = getNum(userInput);
+								System.out.print("Enter the new value: ");
+								int el04 = getNum(userInput);
+								System.out.print("Enter the new ETA: ");
+								String el05 = getString(userInput);
+								System.out.print("Enter the new arrival date: ");
+								String el06 = getString(userInput);
+								System.out.print("Enter the new warehouse number: ");
+								int el07 = getNum(userInput);
+								try {
+									PreparedStatement newElementOrder = conn.prepareStatement(sql);
+									newElementOrder.setString(1, el01);
+									newElementOrder.setString(2, el02);
+									newElementOrder.setInt(3, el03);
+									newElementOrder.setInt(4, el04);
+									newElementOrder.setString(5, el05);
+									newElementOrder.setString(6, el06);
+									newElementOrder.setInt(7, el07);
+									newElementOrder.execute();
+									System.out.println("Element order info edited.");
+    								} catch (SQLException e) {
+									e.printStackTrace();
+    								}
     							break;
     						case "9":
     							System.out.println("--- Edit Review ---");
-    							//TODO
+    							System.out.print("Enter the current review order ID: ");
+								String r00 = getString(userInput);
+    							sql = "UPDATE Review SET Date = ?, Title = ?, Content = ?, Rate = ?, Order_ID = ? WHERE Order_ID = '"+r00+"';";
+								System.out.print("Enter the new date: ");
+								String r01 = getString(userInput);
+								System.out.print("Enter the new title: ");
+								String r02 = getString(userInput);
+								System.out.print("Enter the new content: ");
+								String r03 = getString(userInput);
+								System.out.print("Enter the new rating: ");
+								int r04 = getNum(userInput);
+								System.out.print("Enter the new order ID: ");
+								String r05 = getString(userInput);
+								try {
+									PreparedStatement newReview = conn.prepareStatement(sql);
+									newReview.setString(1, r01);
+									newReview.setString(2, r02);
+									newReview.setString(3, r03);
+									newReview.setInt(4, r04);
+									newReview.setString(5, r05);
+									newReview.execute();
+									System.out.println("Review info edited.");
+    								} catch (SQLException e) {
+									e.printStackTrace();
+    								}
     							break;
     						case "10":
     							System.out.println("--- Edit City ---");
-    							//TODO
+    							System.out.print("Enter the current city to edit: ");
+								String c00 = getString(userInput);
+								System.out.print("Enter the current state to edit: ");
+								String c000 = getString(userInput);
+								sql = "UPDATE City SET City_Name = ?, State = ? WHERE City_Name = '"+c00+"' AND State = '"+c000+"';";
+								System.out.print("Enter the new city: ");
+								String c01 = getString(userInput);
+								System.out.print("Enter the new state: ");
+								String c02 = getString(userInput);
+								try {
+									PreparedStatement newCity = conn.prepareStatement(sql);
+									newCity.setString(1, c01);
+									newCity.setString(2, c02);
+									newCity.execute();
+									System.out.println("City info edited.");
+    								} 
+								catch (SQLException e) {
+									e.printStackTrace();
+    								}
     							break;
     						default: 
     							System.out.println("Error: Invalid Input");
@@ -749,9 +1015,7 @@ public class Checkpoint4 {
     								System.out.println("Error: Invalid Input");
     								break;						
     						}
-    						
     						break;
-    					
     					// For when the user enters a value not accepted by the main menu			
     					default:
     						System.out.println("Error: Invalid Input");
